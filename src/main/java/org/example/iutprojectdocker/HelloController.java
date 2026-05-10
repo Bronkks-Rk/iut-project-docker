22f4330985ea7383c64ae409c732b0afaec800b3
Bronkks-Rk <rayan.essaidi8@etu.univ-lorraine.fr>
2026-05-10 16:47:21 +0200

    Update HelloController.java

--- src/main/java/org/example/iutprojectdocker/HelloController.java
+++ src/main/java/org/example/iutprojectdocker/HelloController.java
@@ -3,6 +3,8 @@ package org.example.iutprojectdocker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import redis.clients.jedis.Jedis;
+
 @RestController
 public class HelloController {
 
