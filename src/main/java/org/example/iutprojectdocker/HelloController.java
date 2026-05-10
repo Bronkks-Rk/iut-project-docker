1645c81458e65ab47276e90fdaa669c5cc504471
Bronkks-Rk <rayan.essaidi8@etu.univ-lorraine.fr>
2026-05-10 22:29:20 +0200

    Résolution du conflit

--- src/main/java/org/example/iutprojectdocker/HelloController.java
+++ src/main/java/org/example/iutprojectdocker/HelloController.java
@@ -26,21 +26,6 @@ public class HelloController {
             }
                 catch (Exception e) {
 
-    /**
-     * @return A greeting message that includes the value of the "APP_USER" environment variable, or "World" if it is not set.
-     */
-    @GetMapping("/")
-    public String hello() {
-        try {
-            String appUser = System.getenv("APP_USER");
-            String name = (appUser == null || appUser.isEmpty()) ? "World" : appUser;
-
-            String base = String.format("Hello %s!", name);
-            return base;
-
-        } catch (Exception e) {
-
-            return "Error: " + e.getMessage();
         }
     }
 }
