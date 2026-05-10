9f0ae7630f1bad5911e7d6ee05d7b2e8d5b5dd69
Bronkks-Rk <rayan.essaidi8@etu.univ-lorraine.fr>
2026-05-10 16:52:52 +0200

    Update HelloController.java

--- src/main/java/org/example/iutprojectdocker/HelloController.java
+++ src/main/java/org/example/iutprojectdocker/HelloController.java
@@ -8,11 +8,19 @@ import redis.clients.jedis.Jedis;
 @RestController
 public class HelloController {
 
-
     @GetMapping("/")
     public String hello() {
+
+        //Récupération du nom
+        String appUser = System.getenv("APP_USER");
+        
+        String name = (appUser == null || appUser.isEmpty()) ? "World" : appUser;
+
+
+        
         try {
             return "Hello World";
+            
         } catch (Exception e) {
             return "Error: " + e.getMessage();
         }
